051672628c8e36e0422284840429850a9d3da03b
CARLOS JUAN ORELLANA <cjorellana@gmail.com>
2023-06-02 15:03:44 -0600

    se agrega

--- eventos/views.py
+++ eventos/views.py
@@ -1,3 +1,17 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def index(request):
+    data = {
+        'titulo': "Principal"
+    }
+
+    return render(request,"index.html",data)
+
+def about(request):
+    data = {
+        'titulo': "About...",
+        'contenido': "esta es la pagina de about"
+    }
+    return render(request,"about.html",data)
